47ca041b007c0bf2a53e5085cb67e0f7228273d8
Sadou <sadouship@gmail.com>
2020-01-16 13:02:09 +0100

    Updates

--- aide_sante/config/database.php
+++ aide_sante/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'AA6LjfIgzI'),
+            'username' => env('DB_USERNAME', 'AA6LjfIgzI'),
+            'password' => env('DB_PASSWORD', '4VMsoo358A'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
